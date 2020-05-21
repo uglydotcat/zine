a63871b1e5359110614d87e5590d12e0b99b3589
Biel <bielp07@gmail.com>
2020-05-21 14:22:19 -0300

    Initial Commit

---
+++ app/src/main/java/ugly/cat/zine/MainActivity.kt
@@ -0,0 +1,19 @@
+package ugly.cat.zine
+
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            window.decorView.systemUiVisibility =
+                window.decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+    }
+}
